7a417d36504160ab215ebcb7a2d90eb28fa3aa93
Yves Courtois <Courtois1337@gmail.com>
2014-01-05 21:32:50 -0600

    a whole bunch of changes

--- includes/ready.js
+++ includes/ready.js
@@ -7,12 +7,10 @@ $(document).ready(function(){
 	else{
 		$('#wrap').css("padding","0px");
 	}
-	$('.banner').unslider({
-		speed: 1000,               //  The speed to animate each slide (in milliseconds)
-	delay: 3000,              //  The delay between slide animations (in milliseconds)
-	complete: function() {},  //  A function that gets called after every slide animation
-	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-	dots: false,               //  Display dot navigation
-	fluid: true              //  Support responsive design. May break non-responsive designs
+	$('#slideshow').fadeSlideShow({
+		PlayPauseElement: false,
+		NextElement: false,
+		PrevElement: false,
+		ListElement: false
 	});
 });
